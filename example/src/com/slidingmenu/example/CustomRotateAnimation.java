0f15704031bb3957f9d2a3290a631c987fb4cde6
jfeinstein10 <j.feinstein10@gmail.com>
2012-09-03 11:15:50 -0400

    Interpolator for the rotate animation

--- example/src/com/slidingmenu/example/CustomRotateAnimation.java
+++ example/src/com/slidingmenu/example/CustomRotateAnimation.java
@@ -2,20 +2,30 @@ package com.slidingmenu.example;
 
 import android.graphics.Canvas;
 import android.os.Bundle;
+import android.view.animation.Interpolator;
 
 import com.slidingmenu.lib.SlidingMenu.CanvasTransformer;
 
 public class CustomRotateAnimation extends CustomAnimation {
 
+	private static Interpolator interp = new Interpolator() {
+		@Override
+		public float getInterpolation(float t) {
+			t -= 1.0f;
+			return t * t * t + 1.0f;
+		}		
+	};
+
 	public CustomRotateAnimation() {
 		super(R.string.anim_rot, new CanvasTransformer() {
 			@Override
 			public void transformCanvas(Canvas canvas, float percentOpen) {
-				canvas.rotate(180.0f*(1-percentOpen), canvas.getWidth(), canvas.getHeight()/2);
+				canvas.rotate(180.0f*(1-interp.getInterpolation(percentOpen)), 
+						canvas.getWidth(), canvas.getHeight()/2);
 			}
 		});
 	}
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
