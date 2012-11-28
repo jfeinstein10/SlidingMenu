507ca04de30a82ca13089538bd420d833d821952
jfeinstein10 <j.feinstein10@gmail.com>
2012-11-28 14:07:18 -0500

    Cleaning up left and right

--- example/src/com/slidingmenu/example/LeftAndRightActivity.java
+++ example/src/com/slidingmenu/example/LeftAndRightActivity.java
@@ -13,8 +13,6 @@ import com.slidingmenu.lib.SlidingMenu.OnOpenedListener;
 
 public class LeftAndRightActivity extends BaseActivity {
 
-//	private SlidingMenu mRight;
-
 	public LeftAndRightActivity() {
 		super(R.string.left_and_right);
 	}
@@ -24,27 +22,6 @@ public class LeftAndRightActivity extends BaseActivity {
 		super.onCreate(savedInstanceState);
 		getSlidingMenu().setMode(SlidingMenu.LEFT_RIGHT);
 		getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
-
-//		mRight = new SlidingMenu(this);		
-//		mRight.setMode(SlidingMenu.RIGHT);
-//		mRight.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
-//		mRight.setShadowWidthRes(R.dimen.shadow_width);
-//		mRight.setShadowDrawable(R.drawable.shadowright);
-//		mRight.setBehindOffsetRes(R.dimen.slidingmenu_offset);
-//		mRight.setFadeDegree(0.35f);
-//		mRight.setMenu(R.layout.menu_frame_two);
-//		mRight.setOnOpenedListener(new OnOpenedListener() {
-//			@Override
-//			public void onOpened() {
-//				getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
-//			}
-//		});
-//		mRight.setOnClosedListener(new OnClosedListener() {
-//			@Override
-//			public void onClosed() {
-//				getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
-//			}			
-//		});
 		
 		setContentView(R.layout.content_frame);
 		getSupportFragmentManager()
@@ -57,17 +34,6 @@ public class LeftAndRightActivity extends BaseActivity {
 		.beginTransaction()
 		.replace(R.id.menu_frame_two, new SampleListFragment())
 		.commit();
-	}
-	
-	@Override
-	public void onPostCreate(Bundle savedInstanceState) {
-		super.onPostCreate(savedInstanceState);
-
-//		View v = getSlidingMenu().getContent();
-//		((ViewGroup) v.getParent()).removeView(v);
-//		mRight.setContent(v);
-//		getSlidingMenu().setContent(mRight);
-
 	}
 
 }
