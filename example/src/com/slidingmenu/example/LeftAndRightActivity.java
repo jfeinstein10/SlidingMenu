f211301e892bbf19c2aca8fa9151ed03c95cb397
jfeinstein10 <j.feinstein10@gmail.com>
2012-11-28 01:24:06 -0500

    Updated LeftAndRightActivity

--- example/src/com/slidingmenu/example/LeftAndRightActivity.java
+++ example/src/com/slidingmenu/example/LeftAndRightActivity.java
@@ -1,11 +1,11 @@
 package com.slidingmenu.example;
 
-import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.slidingmenu.example.fragments.ColorFragment;
 import com.slidingmenu.lib.SlidingMenu;
 import com.slidingmenu.lib.SlidingMenu.OnClosedListener;
 import com.slidingmenu.lib.SlidingMenu.OnOpenedListener;
@@ -13,7 +13,7 @@ import com.slidingmenu.lib.SlidingMenu.OnOpenedListener;
 
 public class LeftAndRightActivity extends BaseActivity {
 
-	private SlidingMenu mRight;
+//	private SlidingMenu mRight;
 
 	public LeftAndRightActivity() {
 		super(R.string.left_and_right);
@@ -22,34 +22,40 @@ public class LeftAndRightActivity extends BaseActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		getSlidingMenu().setMode(SlidingMenu.LEFT);
-		getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
+		getSlidingMenu().setMode(SlidingMenu.LEFT_RIGHT);
+		getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
 
-		mRight = new SlidingMenu(this);		
-		mRight.setMode(SlidingMenu.RIGHT);
-		mRight.setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
-		mRight.setShadowWidthRes(R.dimen.shadow_width);
-		mRight.setShadowDrawable(R.drawable.shadowright);
-		mRight.setBehindOffsetRes(R.dimen.slidingmenu_offset);
-		mRight.setFadeDegree(0.35f);
-		mRight.setMenu(R.layout.menu_frame_two);
-		mRight.setOnOpenedListener(new OnOpenedListener() {
-			@Override
-			public void onOpened() {
-				getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
-			}
-		});
-		mRight.setOnClosedListener(new OnClosedListener() {
-			@Override
-			public void onClosed() {
-				getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
-			}			
-		});
+//		mRight = new SlidingMenu(this);		
+//		mRight.setMode(SlidingMenu.RIGHT);
+//		mRight.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+//		mRight.setShadowWidthRes(R.dimen.shadow_width);
+//		mRight.setShadowDrawable(R.drawable.shadowright);
+//		mRight.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+//		mRight.setFadeDegree(0.35f);
+//		mRight.setMenu(R.layout.menu_frame_two);
+//		mRight.setOnOpenedListener(new OnOpenedListener() {
+//			@Override
+//			public void onOpened() {
+//				getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
+//			}
+//		});
+//		mRight.setOnClosedListener(new OnClosedListener() {
+//			@Override
+//			public void onClosed() {
+//				getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+//			}			
+//		});
 		
 		setContentView(R.layout.content_frame);
 		getSupportFragmentManager()
 		.beginTransaction()
 		.replace(R.id.content_frame, new SampleListFragment())
+		.commit();
+		
+		getSlidingMenu().setSecondaryMenu(R.layout.menu_frame_two);
+		getSupportFragmentManager()
+		.beginTransaction()
+		.replace(R.id.menu_frame_two, new SampleListFragment())
 		.commit();
 	}
 	
@@ -57,30 +63,11 @@ public class LeftAndRightActivity extends BaseActivity {
 	public void onPostCreate(Bundle savedInstanceState) {
 		super.onPostCreate(savedInstanceState);
 
-		View v = getSlidingMenu().getContent();
-		((ViewGroup) v.getParent()).removeView(v);
-		mRight.setContent(v);
-		getSlidingMenu().setContent(mRight);
+//		View v = getSlidingMenu().getContent();
+//		((ViewGroup) v.getParent()).removeView(v);
+//		mRight.setContent(v);
+//		getSlidingMenu().setContent(mRight);
 
-		getSupportFragmentManager()
-		.beginTransaction()
-		.replace(R.id.menu_frame_two, new SampleListFragment())
-		.commit();
-		// get the window background
-//		TypedArray a = getTheme().obtainStyledAttributes(new int[] {android.R.attr.windowBackground});
-//		int background = a.getResourceId(0, 0);
-//		a.recycle();
-//		View content = getLayoutInflater().inflate(R.layout.content_frame, null);
-//		content.setBackgroundResource(background);
-	}
-
-	@Override
-	public boolean onKeyUp(int keyCode, KeyEvent event) {
-		if (keyCode == KeyEvent.KEYCODE_BACK && mRight.isBehindShowing()) {
-			mRight.showAbove();
-			return true;
-		}
-		return super.onKeyUp(keyCode, event);
 	}
 
 }
