9b7d10a2ff132f6f367d1a90251b67af6a5b5918
jfeinstein10 <j.feinstein10@gmail.com>
2012-12-19 22:21:13 -0500

    Fixing eclipse imports

--- example/src/com/slidingmenu/example/ExampleListActivity.java
+++ example/src/com/slidingmenu/example/ExampleListActivity.java
@@ -35,6 +35,7 @@ public class ExampleListActivity extends SherlockPreferenceActivity {
 		super.onCreate(savedInstanceState);
 		setTitle(R.string.app_name);
 		
+//		Crittercism.init(getApplicationContext(), "508ab27601ed857a20000003");
 		this.addPreferencesFromResource(R.xml.main);
 	}
 
