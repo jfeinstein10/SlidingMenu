e988e37f8e4ddad280e83cd34200a08ccf4980f3
bdonahue <bill@snipsnap.it>
2013-07-29 13:55:31 -0400

    Fixing bug where the compat library will sometimes throw error when trying to get the motionevent.

--- library/src/com/jeremyfeinstein/slidingmenu/lib/CustomViewAbove.java
+++ library/src/com/jeremyfeinstein/slidingmenu/lib/CustomViewAbove.java
@@ -782,20 +782,25 @@ public class CustomViewAbove extends ViewGroup {
 		final int pointerIndex = getPointerIndex(ev, activePointerId);
 		if (activePointerId == INVALID_POINTER)
 			return;
-		final float x = MotionEventCompat.getX(ev, pointerIndex);
-		final float dx = x - mLastMotionX;
-		final float xDiff = Math.abs(dx);
-		final float y = MotionEventCompat.getY(ev, pointerIndex);
-		final float dy = y - mLastMotionY;
-		final float yDiff = Math.abs(dy);
-		if (xDiff > (isMenuOpen()?mTouchSlop/2:mTouchSlop) && xDiff > yDiff && thisSlideAllowed(dx)) {		
-			startDrag();
-			mLastMotionX = x;
-			mLastMotionY = y;
-			setScrollingCacheEnabled(true);
-			// TODO add back in touch slop check
-		} else if (xDiff > mTouchSlop) {
-			mIsUnableToDrag = true;
+		try {
+	        final float x = MotionEventCompat.getX(ev, pointerIndex);
+	        final float dx = x - mLastMotionX;
+	        final float xDiff = Math.abs(dx);
+	        final float y = MotionEventCompat.getY(ev, pointerIndex);
+	        final float dy = y - mLastMotionY;
+	        final float yDiff = Math.abs(dy);
+	        if (xDiff > (isMenuOpen()?mTouchSlop/2:mTouchSlop) && xDiff > yDiff && thisSlideAllowed(dx)) {      
+	            startDrag();
+	            mLastMotionX = x;
+	            mLastMotionY = y;
+	            setScrollingCacheEnabled(true);
+	            // TODO add back in touch slop check
+	        } else if (xDiff > mTouchSlop) {
+	            mIsUnableToDrag = true;
+	        }
+		} catch (IllegalArgumentException e) {
+		    // Compatibility library sometimes crashes internally on calls to MotionEventCompat
+		    return;
 		}
 	}
 
