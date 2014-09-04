73f040e0420a8eff0662fa239d80e35b0bc0b729
brstgt <brstgt@googlemail.com>
2014-09-04 12:03:52 +0200

    Catch ArrayIndexOutOfBoundsException thrown by android framework

--- library/src/com/jeremyfeinstein/slidingmenu/lib/CustomViewAbove.java
+++ library/src/com/jeremyfeinstein/slidingmenu/lib/CustomViewAbove.java
@@ -779,21 +779,27 @@ public class CustomViewAbove extends ViewGroup {
 		final int pointerIndex = getPointerIndex(ev, activePointerId);
 		if (activePointerId == INVALID_POINTER || pointerIndex == INVALID_POINTER)
 			return;
-		final float x = MotionEventCompat.getX(ev, pointerIndex);
-		final float dx = x - mLastMotionX;
-		final float xDiff = Math.abs(dx);
-		final float y = MotionEventCompat.getY(ev, pointerIndex);
-		final float dy = y - mLastMotionY;
-		final float yDiff = Math.abs(dy);
-		if (xDiff > (isMenuOpen()?mTouchSlop/2:mTouchSlop) && xDiff > yDiff && thisSlideAllowed(dx)) {		
-			startDrag();
-			mLastMotionX = x;
-			mLastMotionY = y;
-			setScrollingCacheEnabled(true);
-			// TODO add back in touch slop check
-		} else if (xDiff > mTouchSlop) {
-			mIsUnableToDrag = true;
-		}
+        try {
+            final float x = MotionEventCompat.getX(ev, pointerIndex);
+            final float dx = x - mLastMotionX;
+            final float xDiff = Math.abs(dx);
+            final float y = MotionEventCompat.getY(ev, pointerIndex);
+            final float dy = y - mLastMotionY;
+            final float yDiff = Math.abs(dy);
+            if (xDiff > (isMenuOpen()?mTouchSlop/2:mTouchSlop) && xDiff > yDiff && thisSlideAllowed(dx)) {
+                startDrag();
+                mLastMotionX = x;
+                mLastMotionY = y;
+                setScrollingCacheEnabled(true);
+                // TODO add back in touch slop check
+            } else if (xDiff > mTouchSlop) {
+                mIsUnableToDrag = true;
+            }
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            // Ignore. Occasionally happens in MotionEventCompat.getX
+            // Known problem, caused in Android Framework
+        }
 	}
 
 	@Override
